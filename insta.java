552ee496d7ff4dfefe5de6ebabb6f0887eed4ef5
root <root@ip-172-31-32-148.us-east-2.compute.internal>
2026-05-16 09:13:52 +0000

    This my Third Commit

--- insta.java
+++ insta.java
@@ -1,14 +1,2 @@
 This is My New Github Machine....
-public class GFG{
-    public static void main(String[] args){
-        int m = 9, n = 5;
-        System.out.println("Before swapping: m = " + m + ", n = " + n);
- 
-        int temp = m;
-        m = n;
-        n = temp;
- 
-        System.out.println("After swapping: m = " + m + ", n = " + n);
-    }
-
-    Newest Version
+ Welcome to the World of Kanivel!!!
